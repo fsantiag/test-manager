b7047bd09a6bda21dfd1c69cc4cdd08ae328a03b
Felipe Santiago <jfelipe.sfilho@gmail.com>
2017-04-13 16:43:28 -0300

    Fix deprecated use of function on sample test

--- autotests/tests/sample_false_assert.py
+++ autotests/tests/sample_false_assert.py
@@ -1,8 +1,7 @@
-import time
 from unittest import TestCase
 
 
 class Sample(TestCase):
     def test_sameple_with_big_timeout(self):
         print("Testing false assert")
-        self.assertEquals(1, 2)
+        self.assertEqual(1, 2)
