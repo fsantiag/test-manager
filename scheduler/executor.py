b98b1056e6489f78bb9d2d6e046a34b25ddca647
Felipe Santiago <jfelipe.sfilho@gmail.com>
2017-04-13 11:45:07 -0300

    Add link to check test output

--- scheduler/executor.py
+++ scheduler/executor.py
@@ -11,7 +11,7 @@ class TestExecutor(threading.Thread):
         self.queue = queue
 
     def run(self):
-        with open("autotests/runs/{}".format(self.run_id), "w") as f:
+        with open("runs/static/runs/autotests/runs/{}.txt".format(self.run_id), "w") as f:
             CMD = "python3 -m unittest -v autotests/tests/{}.py 2>&1".format(
                     self.test_name)
             return_code = subprocess.call(CMD, stdout=f, shell=True)
